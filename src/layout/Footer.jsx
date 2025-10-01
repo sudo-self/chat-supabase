3e122fdee39d5fe8f75c6625430098955f5864e6
sudo-self <tech@jessejesse.com>
2025-10-01 08:32:06 -0600

    Update Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,15 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Link, useColorModeValue } from "@chakra-ui/react";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import MessageForm from "../components/MessageForm";
+import { useColorModeValue as getColor } from "@/components/ui/color-mode";
 
 export default function Footer() {
+  const bg = getColor("white", "gray.800");
+  const textColor = getColor("gray.800", "gray.100");
+  const linkHover = getColor("gray.600", "gray.300");
+
   return (
-    <Box position="fixed" bottom="0" width="100%">
+    <Box position="fixed" bottom="0" width="100%" bg={bg} color={textColor}>
       <MessageForm />
 
       <Grid
@@ -14,21 +19,37 @@ export default function Footer() {
         py="4px"
         px="30px"
         height="40px"
-        bg="white"
       >
         <GridItem justifySelf="start">
-          <a href="https://x.com/lightfighter719" target="_blank" rel="noreferrer">
-            <FaTwitter style={{ display: "inline" }} /> lightfighter719
-          </a>
+          <Link
+            href="https://x.com/lightfighter719"
+            target="_blank"
+            rel="noreferrer"
+            _hover={{ color: linkHover }}
+            display="inline-flex"
+            alignItems="center"
+            gap="2px"
+          >
+            <FaTwitter /> lightfighter719
+          </Link>
         </GridItem>
         <GridItem justifySelf="end">
-          <a href="https://github.com/sudo-self/chat-supabase" target="_blank" rel="noreferrer">
-            <FaGithub style={{ display: "inline" }} /> Source code
-          </a>
+          <Link
+            href="https://github.com/sudo-self/chat-supabase"
+            target="_blank"
+            rel="noreferrer"
+            _hover={{ color: linkHover }}
+            display="inline-flex"
+            alignItems="center"
+            gap="2px"
+          >
+            <FaGithub /> Source code
+          </Link>
         </GridItem>
       </Grid>
     </Box>
   );
 }
+
 
 
