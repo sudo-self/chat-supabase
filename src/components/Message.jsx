64d01ac5c2bfa119ad7353c7fff768ee1fe899fc
sudo-self <tech@jessejesse.com>
2025-10-01 07:57:06 -0600

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,20 +1,21 @@
 import { Box, Grid, GridItem } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
 import { MdVerified } from "react-icons/md";
 import { truncateText } from "../utils";
 
+dayjs.extend(relativeTime);
+
 export default function Message({ message, isYou }) {
-  const countyCode =
+  const countryCode =
     message.country && message.country !== "undefined"
       ? message.country.toLowerCase()
       : "";
+
   return (
     <Box display="grid" justifyItems={isYou ? "end" : "start"}>
       <Grid
         templateRows="30px 1fr 25px"
-        templateColumns="1fr"
         w="70%"
         px="3"
         py="2"
@@ -25,12 +26,12 @@ export default function Message({ message, isYou }) {
         mt="5"
         position="relative"
         _after={{
-          position: "absolute",
           content: "''",
+          position: "absolute",
           width: 0,
           height: 0,
           borderStyle: "solid",
-          borderWidth: isYou ? "0px 0px 10px 10px" : "0px 10px 10px 0",
+          borderWidth: isYou ? "0 0 10px 10px" : "0 10px 10px 0",
           borderColor: isYou
             ? "transparent transparent transparent #dbfff9"
             : "transparent #edf3f9 transparent transparent",
@@ -46,24 +47,27 @@ export default function Message({ message, isYou }) {
           color="gray.500"
           mb="2"
         >
-          <span>{message.username} </span>
+          <span>{message.username}</span>
           {message.is_authenticated && (
             <MdVerified
               color="#1d9bf0"
-              style={{ display: "inline", marginRight: "5px" }}
+              style={{ display: "inline", marginLeft: "5px" }}
             />
           )}
-          {countyCode && (
-            <Box display="inline-block" fontSize="10px">
+          {countryCode && (
+            <Box display="inline-block" fontSize="10px" ml="2">
               from {message.country}{" "}
               <img
-                style={{ display: "inline-block", marginTop: "-4px" }}
-                src={`/flags/${countyCode}.png`}
+                style={{ display: "inline-block", marginTop: "-4px", marginLeft: "2px" }}
+                src={`/flags/${countryCode}.png`}
                 alt={message.country}
+                width={14}
+                height={10}
               />
             </Box>
           )}
         </GridItem>
+
         <GridItem
           justifySelf="start"
           textAlign="left"
@@ -73,6 +77,7 @@ export default function Message({ message, isYou }) {
         >
           {truncateText(message.text)}
         </GridItem>
+
         <GridItem
           color="gray"
           fontSize="10px"
@@ -85,3 +90,4 @@ export default function Message({ message, isYou }) {
     </Box>
   );
 }
+
