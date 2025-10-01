e9b3324d00ec6baa2ace253652dd7e01feb63764
sudo-self <tech@jessejesse.com>
2025-10-01 08:22:36 -0600

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,35 +1,49 @@
 import { Alert, Box, Button, Spinner } from "@chakra-ui/react";
 import { useAppContext } from "../context/appContext";
 import Message from "./Message";
+import { useColorModeValue } from "@/components/ui/color-mode";
 
 export default function Messages() {
   const { username, loadingInitial, error, getMessagesAndSubscribe, messages } =
     useAppContext();
+
   const reversed = [...messages].reverse();
+  const alertBg = useColorModeValue("red.100", "red.700");
+  const alertColor = useColorModeValue("red.800", "red.100");
+
   if (loadingInitial)
     return (
-      <Box textAlign="center">
-        <Spinner />
+      <Box textAlign="center" py="20px">
+        <Spinner size="xl" />
       </Box>
     );
+
   if (error)
     return (
-      <Alert.Root status="error" mt="20px">
+      <Box
+        mt="20px"
+        p="3"
+        borderRadius="8px"
+        bg={alertBg}
+        color={alertColor}
+        textAlign="center"
+      >
         {error}
         <Button
-          ml="5px"
-          onClick={getMessagesAndSubscribe}
+          ml="8px"
+          size="sm"
           colorScheme="red"
-          variant="link"
+          variant="outline"
+          onClick={getMessagesAndSubscribe}
         >
-          try to reconnect
+          Retry
         </Button>
-      </Alert.Root>
+      </Box>
     );
 
   if (!messages.length)
     return (
-      <Box as="h3" textAlign="center">
+      <Box as="h3" textAlign="center" py="20px" color={useColorModeValue("gray.600", "gray.300")}>
         No messages 😞
       </Box>
     );
@@ -39,3 +53,4 @@ export default function Messages() {
     return <Message key={message.id} message={message} isYou={isYou} />;
   });
 }
+
